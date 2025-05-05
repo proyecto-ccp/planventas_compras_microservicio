9ca3c3bef5dd110a7ea69eeba77541a7da94e83d
EverUniAndes <eo.martinez2@uniandes.edu.co>
2025-05-04 21:38:20 -0500

    incluir asociaciones productos y vendedores

---
+++ PlanesVentas.Dominio/Servicios/Vendedores/AgregarVendedor.cs
@@ -0,0 +1,19 @@
+﻿
+
+using PlanesVentas.Dominio.Entidades;
+using PlanesVentas.Dominio.Puertos.Repositorios;
+
+namespace PlanesVentas.Dominio.Servicios.Vendedores
+{
+    public class AgregarVendedor(IVendedoresRepositorio vendedoresRepositorio)
+    {
+        private readonly IVendedoresRepositorio _vendedoresRepositorio = vendedoresRepositorio;
+
+        public async Task<VendedorPlanVenta> Ejecutar(VendedorPlanVenta vendedor)
+        {
+            vendedor.FechaCreacion = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Unspecified);
+            return await _vendedoresRepositorio.Agregar(vendedor);
+            
+        }
+    }
+}
---
+++ PlanesVentas.Dominio/Servicios/Vendedores/ConsultarVendedor.cs
@@ -0,0 +1,17 @@
+﻿
+
+using PlanesVentas.Dominio.Entidades;
+using PlanesVentas.Dominio.Puertos.Repositorios;
+
+namespace PlanesVentas.Dominio.Servicios.Vendedores
+{
+    public class ConsultarVendedor(IVendedoresRepositorio vendedoresRepositorio)
+    {
+        private readonly IVendedoresRepositorio _vendedoresRepositorio = vendedoresRepositorio;
+        public async Task<List<VendedorPlanVenta>> Ejecutar(Guid id)
+        {
+            return await _vendedoresRepositorio.ObtenerPorPlanVenta(id);
+        }
+
+    }
+}
